f41cd79379736fcdf1c0041c660cd57d486cae63
te1 <te1@users.noreply.github.com>
2021-07-15 02:09:50 +0200

    crop monster images

---
+++ scripts/crop.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+function getFileNames(directory) {
+  let result = [];
+
+  let filePath, stat;
+
+  fs.readdirSync(directory).forEach((fileName) => {
+    filePath = path.join(directory, fileName);
+    stat = fs.statSync(filePath);
+
+    if (stat.isFile()) {
+      result.push(fileName);
+    }
+  });
+
+  return result;
+}
+
+(async () => {
+  const raw = './monsters/raw/';
+  const cropped = './monsters/cropped/';
+
+  for (const fileName of getFileNames(raw)) {
+    await sharp(raw + fileName)
+      .png()
+      .extract({
+        left: 25,
+        top: 130,
+        width: 525,
+        height: 465,
+      })
+      .toFile(cropped + fileName.replace(/\.jpg$/, '.png'));
+  }
+})();
